93beeec031764cf730fcc8ddf076806368a684d7
lyxal <36217120+lyxal@users.noreply.github.com>
2024-11-24 17:35:16 +1100

    Bump Vyxal to 2.22.4.3 (I did a silly with 2.22.4) (#47)
    * Bump Vyxal to 2.22.4.2 (I did a silly with 2.22.4)
    
    * Update langs.rs

--- common/src/langs.rs
+++ common/src/langs.rs
@@ -66,6 +66,6 @@ pub const LANGS: &[Lang] = &[
         plugin: "https://github.com/lyxal/vyxasdf.git",
         env: &[],
         install_env: &[],
-        latest_version: "2.22.4",
+        latest_version: "2.22.4.3",
     },
 ];
